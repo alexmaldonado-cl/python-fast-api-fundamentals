ef8b4b4e3a0198b672708cb9799cdb9df6eeca7a
Alex Maldonado <alex.maldonado@outlook.com>
2023-02-01 08:59:14 -0300

    Modify docs info

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app         = FastAPI()
+app.title   = 'My FastAPI App'
+app.version = "0.0.1"
+# app.contact = {
+#     "name": "Alex Maldonado",
+#     "url": "https://github.com/alexmaldonado-cl/",
+#     "email": "alex.maldonado@outlook.com"
+# }
 
-@app.get('/')
+@app.get('/', tags=['home'])
 def message():
     return "Hello World!"
