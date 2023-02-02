fdd937558f2eda79c279a1b011125e2dd034f0b0
Alex Maldonado <alex.maldonado@outlook.com>
2023-02-02 08:50:48 -0300

    Added get routes with parameters

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app         = FastAPI()
 app.title   = 'My FastAPI App'
@@ -9,6 +10,36 @@ app.version = "0.0.1"
 #     "email": "alex.maldonado@outlook.com"
 # }
 
+movies = [
+    {
+		"id": 1,
+		"title": "Avatar",
+		"overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+		"year": "2009",
+		"rating": 7.8,
+		"category": "Acción"
+	},
+    {
+		"id": 2,
+		"title": "Avatar",
+		"overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+		"year": "2009",
+		"rating": 7.8,
+		"category": "Acción"
+	}
+]
+
 @app.get('/', tags=['home'])
 def message():
-    return "Hello World!"
+    return HTMLResponse('<h1>Hello World</h1>')
+
+@app.get('/movies', tags=['movies'])
+def get_movies():
+    return movies
+
+@app.get('/movies/{id}', tags=['movies'])
+def get_movie(id: int):
+	result = filter(lambda item : item['id'] == id, movies)
+	result = list(result)
+    
+	return result
